0c311bd434f1bfa280c6b3577b70385a7864d764
Jerry Ryle <jerry@mindtribe.com>
2015-04-25 21:07:43 -0700

    Updated example to show array-style flag specification.

--- examples/LibraryWithMultipleApps/ExampleLibrary/mtbuildfile.rb
+++ examples/LibraryWithMultipleApps/ExampleLibrary/mtbuildfile.rb
@@ -5,9 +5,8 @@ static_library_project :ExampleLibrary, File.dirname(__FILE__) do |lib|
   lib.add_configuration :Configuration1,
     sources: ['src/**/*.c'],
     toolchain: toolchain(:arm_none_eabi_gcc,
-      cppflags: "-Dgcc",
-      cflags: '-std=c99 -mcpu=cortex-m4 -mthumb -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Werror -Wextra -pedantic-errors',
-      cxxflags: '-mcpu=cortex-m4 -mthumb -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Werror -Wextra -pedantic-errors',
+      #inherits flags from workspace, but adds to cppflags
+      cppflags: ['-DConfig1'],
       include_paths: ['src']
     ),
     tests: [
@@ -17,9 +16,8 @@ static_library_project :ExampleLibrary, File.dirname(__FILE__) do |lib|
   lib.add_configuration :Configuration2,
     sources: ['src/**/*.c'],
     toolchain: toolchain(:arm_none_eabi_gcc,
-      cppflags: "-Dgcc",
-      cflags: '-std=c99 -mcpu=cortex-m4 -mthumb -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Werror -Wextra -pedantic-errors',
-      cxxflags: '-mcpu=cortex-m4 -mthumb -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Werror -Wextra -pedantic-errors',
+      #inherits flags from workspace, but adds to cppflags
+      cppflags: ['-DConfig2'],
       include_paths: ['src']
     ),
     tests: [
